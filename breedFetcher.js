6f56e39fc42d5ed89991099c40dbe67b75137ca1
Casey Wood <caseywood252@gmail.com>
2021-06-03 22:24:10 +0000

    refactor and lint

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,15 @@
 const request = require('request');
-const arg = process.argv.slice(2);
 
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    if (body) {
+      const data = JSON.parse(body);
+      callback(null, data);
+    }
+  });
+};
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, (error, response, body) => {
-  if (error) console.log('there is an error in the submited URL');
-  if (!body) console.log('Input does not match a cat in the database');
-  if (body) {
-    const data = JSON.parse(body);
-    console.log(data);
-  }
-});
-
-
-
+module.exports = {fetchBreedDescription};
