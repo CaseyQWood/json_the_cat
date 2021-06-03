e30c6930eb4e4dd87e1821bbebcc7e2218ce6073
Casey Wood <caseywood252@gmail.com>
2021-06-03 23:31:46 +0000

    updated mocha and chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,12 +2,10 @@ const request = require('request');
 
 const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-    }
-    if (body) {
+    if (body.length === 2) {callback('this is not a cat', null)}
+    else if(body) {
       const data = JSON.parse(body);
-      callback(null, data);
+      callback(null, data[0].description);
     }
   });
 };
